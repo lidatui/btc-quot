0c5b6230cc310f338df98b77b8d1a31167ff042f
miemiedev <dev@miemie.li>
2013-11-25 22:59:12 +0800

    数据变动高亮

--- btc-quot-frontend/app/js/controllers/quot_controller.js
+++ btc-quot-frontend/app/js/controllers/quot_controller.js
@@ -1,5 +1,6 @@
 (function(ng){
   ng.module('app').controller('QuotController',function($scope,$interval, QuotService){
+    $scope.quotStyle = {};
     $interval(function(){
       QuotService.query().success(function(data){
         if(data.btcChina){
@@ -19,5 +20,62 @@
         }
       });
     },2000);
+
+
+    $scope.$watch('btcChinaQuot.ticker.last', function(newValue, oldValue){
+      if(!newValue && !oldValue){
+        return;
+      }
+      if(newValue > oldValue){
+        $scope.quotStyle.btcChina = 'up';
+      }else{
+        $scope.quotStyle.btcChina = 'down';
+      }
+    });
+
+    $scope.$watch('fxBtcQuot.ticker.last_rate', function(newValue, oldValue){
+      if(!newValue && !oldValue){
+        return;
+      }
+      if(newValue > oldValue){
+        $scope.quotStyle.fxBtc = 'up';
+      }else{
+        $scope.quotStyle.fxBtc = 'down';
+      }
+    });
+
+    $scope.$watch('mtgoxQuot.data.last.value', function(newValue, oldValue){
+      if(!newValue && !oldValue){
+        return;
+      }
+      if(newValue > oldValue){
+        $scope.quotStyle.mtgox = 'up';
+      }else{
+        $scope.quotStyle.mtgox = 'down';
+      }
+    });
+
+    $scope.$watch('bitstampQuot.last', function(newValue, oldValue){
+      if(!newValue && !oldValue){
+        return;
+      }
+      if(newValue > oldValue){
+        $scope.quotStyle.bitstamp = 'up';
+      }else{
+        $scope.quotStyle.bitstamp = 'down';
+      }
+    });
+
+    $scope.$watch('q796Quot.return.last', function(newValue, oldValue){
+      if(!newValue && !oldValue){
+        return;
+      }
+      if(newValue > oldValue){
+        $scope.quotStyle.q796 = 'up';
+      }else{
+        $scope.quotStyle.q796 = 'down';
+      }
+    });
+
   });
 })(angular);
