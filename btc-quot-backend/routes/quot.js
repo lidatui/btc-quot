a24077ef0f2c24f5f778f6120e6946d895e0d392
Miemie Li <dev@miemie.li>
2013-11-26 13:24:16 +0800

    异常处理

--- btc-quot-backend/routes/quot.js
+++ btc-quot-backend/routes/quot.js
@@ -1,4 +1,3 @@
-
 /*
  * GET home page.
  */
@@ -7,101 +6,95 @@ var http = require('http');
 var https = require('https');
 var quot = {};
 
-exports.query = function(req, res){
+exports.query = function (req, res) {
   res.json(quot);
 };
 
-setInterval(function(){
-  try{
-    http.get("http://data.btcchina.com/data/ticker", function(result) {
+var refresh =  function () {
+  try {
+    http.get("http://data.btcchina.com/data/ticker", function (result) {
       var responseParts = [];
       result.setEncoding('utf8');
-      result.on("data", function(chunk) {
+      result.on("data", function (chunk) {
         responseParts.push(chunk);
       });
-      result.on("end", function(){
-        try{
+      result.on("end", function () {
+        try {
           console.log('btcChina -> ' + responseParts.join(''));
           quot.btcChina = JSON.parse(responseParts.join(''));
-        }catch(e){
+        } catch (e) {
           console.error(e);
         }
 
       });
     });
-  }catch(e){ console.error(e); }
 
-  try{
-  https.get("https://data.fxbtc.com/api?op=query_ticker&symbol=btc_cny", function(result) {
-    var responseParts = [];
-    result.setEncoding('utf8');
-    result.on("data", function(chunk) {
-      responseParts.push(chunk);
-    });
-    result.on("end", function(){
-      try{
-        console.log('fxBtc -> ' + responseParts.join(''));
-        quot.fxBtc = JSON.parse(responseParts.join(''));
-      }catch(e){
-        console.error(e);
-      }
-    });
-  });
-  }catch(e){ console.error(e); }
-
-  try{
-  https.get("https://data.mtgox.com/api/2/BTCUSD/money/ticker", function(result) {
-    var responseParts = [];
-    result.setEncoding('utf8');
-    result.on("data", function(chunk) {
-      responseParts.push(chunk);
-    });
-    result.on("end", function(){
-      try{
-        console.log('mtgox -> ' + responseParts.join(''));
-        quot.mtgox = JSON.parse(responseParts.join(''));
-      }catch(e){
-        console.error(e);
-      }
-    });
-  });
-  }catch(e){ console.error(e); }
-
-  try{
-    https.get("https://www.bitstamp.net/api/ticker/", function(result) {
+    https.get("https://data.fxbtc.com/api?op=query_ticker&symbol=btc_cny", function (result) {
       var responseParts = [];
       result.setEncoding('utf8');
-      result.on("data", function(chunk) {
+      result.on("data", function (chunk) {
         responseParts.push(chunk);
       });
-      result.on("end", function(){
-        try{
+      result.on("end", function () {
+        try {
+          console.log('fxBtc -> ' + responseParts.join(''));
+          quot.fxBtc = JSON.parse(responseParts.join(''));
+        } catch (e) {
+          console.error(e);
+        }
+      });
+    });
+
+    https.get("https://data.mtgox.com/api/2/BTCUSD/money/ticker", function (result) {
+      var responseParts = [];
+      result.setEncoding('utf8');
+      result.on("data", function (chunk) {
+        responseParts.push(chunk);
+      });
+      result.on("end", function () {
+        try {
+          console.log('mtgox -> ' + responseParts.join(''));
+          quot.mtgox = JSON.parse(responseParts.join(''));
+        } catch (e) {
+          console.error(e);
+        }
+      });
+    });
+
+    https.get("https://www.bitstamp.net/api/ticker/", function (result) {
+      var responseParts = [];
+      result.setEncoding('utf8');
+      result.on("data", function (chunk) {
+        responseParts.push(chunk);
+      });
+      result.on("end", function () {
+        try {
           console.log('bitstamp -> ' + responseParts.join(''));
           quot.bitstamp = JSON.parse(responseParts.join(''));
-        }catch(e){
+        } catch (e) {
           console.error(e);
         }
       });
     });
-  }catch(e){ console.error(e); }
 
-  try{
-    http.get("http://api.796.com/apiV2/ticker.html?op=futures", function(result) {
+    http.get("http://api.796.com/apiV2/ticker.html?op=futures", function (result) {
       var responseParts = [];
       result.setEncoding('utf8');
-      result.on("data", function(chunk) {
+      result.on("data", function (chunk) {
         responseParts.push(chunk);
       });
-      result.on("end", function(){
-        try{
+      result.on("end", function () {
+        try {
           console.log('q796 -> ' + responseParts.join(''));
           quot.q796 = JSON.parse(responseParts.join(''));
-        }catch(e){
+        } catch (e) {
           console.error(e);
         }
       });
     });
-  }catch(e){ console.error(e); }
+  } catch (e) {
+    console.error(e);
+  }
+}
 
-},10000);
-
+setInterval(refresh, 5000);
